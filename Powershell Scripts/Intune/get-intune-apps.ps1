008d4743cb42df7df2ab3bc399077ca0453db3cb
andrew-s-taylor <59832110+andrew-s-taylor@users.noreply.github.com>
2021-09-21 22:29:57 +0100

    Add files via upload

---
+++ Powershell Scripts/Intune/get-intune-apps.ps1
@@ -0,0 +1,478 @@
+<#
+.SYNOPSIS
+  Displays List of apps from Intune
+.DESCRIPTION
+Display an Intune applist in a grid to find more details
+
+.INPUTS
+None required
+.OUTPUTS
+GridView
+.NOTES
+  Version:        1.0
+  Author:         Andrew Taylor
+  Twitter:        @AndrewTaylor_2
+  WWW:            andrewstaylor.com
+  Creation Date:  11/06/2021
+  Purpose/Change: Initial script development
+  
+.EXAMPLE
+N/A
+#>
+
+####################################################
+
+function Get-AuthToken {
+
+<#
+.SYNOPSIS
+This function is used to authenticate with the Graph API REST interface
+.DESCRIPTION
+The function authenticate with the Graph API Interface with the tenant name
+.EXAMPLE
+Get-AuthToken
+Authenticates you with the Graph API interface
+.NOTES
+NAME: Get-AuthToken
+#>
+
+[cmdletbinding()]
+
+param
+(
+    [Parameter(Mandatory=$true)]
+    $User
+)
+
+$userUpn = New-Object "System.Net.Mail.MailAddress" -ArgumentList $User
+
+$tenant = $userUpn.Host
+
+Write-Host "Checking for AzureAD module..."
+
+    $AadModule = Get-Module -Name "AzureAD" -ListAvailable
+
+    if ($AadModule -eq $null) {
+
+        install-module AzureAD -Scope CurrentUser -AllowClobber -Force
+
+    }
+
+
+# Getting path to ActiveDirectory Assemblies
+# If the module count is greater than 1 find the latest version
+
+    if($AadModule.count -gt 1){
+
+        $Latest_Version = ($AadModule | select version | Sort-Object)[-1]
+
+        $aadModule = $AadModule | ? { $_.version -eq $Latest_Version.version }
+
+            # Checking if there are multiple versions of the same module found
+
+            if($AadModule.count -gt 1){
+
+            $aadModule = $AadModule | select -Unique
+
+            }
+
+        $adal = Join-Path $AadModule.ModuleBase "Microsoft.IdentityModel.Clients.ActiveDirectory.dll"
+        $adalforms = Join-Path $AadModule.ModuleBase "Microsoft.IdentityModel.Clients.ActiveDirectory.Platform.dll"
+
+    }
+
+    else {
+
+        $adal = Join-Path $AadModule.ModuleBase "Microsoft.IdentityModel.Clients.ActiveDirectory.dll"
+        $adalforms = Join-Path $AadModule.ModuleBase "Microsoft.IdentityModel.Clients.ActiveDirectory.Platform.dll"
+
+    }
+
+[System.Reflection.Assembly]::LoadFrom($adal) | Out-Null
+
+[System.Reflection.Assembly]::LoadFrom($adalforms) | Out-Null
+
+$clientId = "d1ddf0e4-d672-4dae-b554-9d5bdfd93547"
+
+$redirectUri = "urn:ietf:wg:oauth:2.0:oob"
+
+$resourceAppIdURI = "https://graph.microsoft.com"
+
+$authority = "https://login.microsoftonline.com/$Tenant"
+
+    try {
+
+    $authContext = New-Object "Microsoft.IdentityModel.Clients.ActiveDirectory.AuthenticationContext" -ArgumentList $authority
+
+    # https://msdn.microsoft.com/en-us/library/azure/microsoft.identitymodel.clients.activedirectory.promptbehavior.aspx
+    # Change the prompt behaviour to force credentials each time: Auto, Always, Never, RefreshSession
+
+    $platformParameters = New-Object "Microsoft.IdentityModel.Clients.ActiveDirectory.PlatformParameters" -ArgumentList "Auto"
+
+    $userId = New-Object "Microsoft.IdentityModel.Clients.ActiveDirectory.UserIdentifier" -ArgumentList ($User, "OptionalDisplayableId")
+
+    $authResult = $authContext.AcquireTokenAsync($resourceAppIdURI,$clientId,$redirectUri,$platformParameters,$userId).Result
+
+        # If the accesstoken is valid then create the authentication header
+
+        if($authResult.AccessToken){
+
+        # Creating header for Authorization token
+
+        $authHeader = @{
+            'Content-Type'='application/json'
+            'Authorization'="Bearer " + $authResult.AccessToken
+            'ExpiresOn'=$authResult.ExpiresOn
+            }
+
+        return $authHeader
+
+        }
+
+        else {
+
+        Write-Host
+        Write-Host "Authorization Access Token is null, please re-run authentication..." -ForegroundColor Red
+        Write-Host
+        break
+
+        }
+
+    }
+
+    catch {
+
+    write-host $_.Exception.Message -f Red
+    write-host $_.Exception.ItemName -f Red
+    write-host
+    break
+
+    }
+
+}
+
+####################################################
+
+Function Get-IntuneApplication(){
+
+<#
+.SYNOPSIS
+This function is used to get applications from the Graph API REST interface
+.DESCRIPTION
+The function connects to the Graph API Interface and gets any applications added
+.EXAMPLE
+Get-IntuneApplication
+Returns any applications configured in Intune
+.NOTES
+NAME: Get-IntuneApplication
+#>
+
+[cmdletbinding()]
+
+param
+(
+    $Name
+)
+
+$graphApiVersion = "Beta"
+$Resource = "deviceAppManagement/mobileApps"
+
+    try {
+
+        if($Name){
+
+        $uri = "https://graph.microsoft.com/$graphApiVersion/$($Resource)"
+        (Invoke-RestMethod -Uri $uri -Headers $authToken -Method Get).Value | Where-Object { ($_.'displayName').contains("$Name") -and (!($_.'@odata.type').Contains("managed")) -and (!($_.'@odata.type').Contains("#microsoft.graph.iosVppApp")) }
+
+        }
+
+        else {
+
+        $uri = "https://graph.microsoft.com/$graphApiVersion/$($Resource)"
+        (Invoke-RestMethod -Uri $uri -Headers $authToken -Method Get).Value | Where-Object { (!($_.'@odata.type').Contains("managed")) -and (!($_.'@odata.type').Contains("#microsoft.graph.iosVppApp")) }
+
+        }
+
+    }
+
+    catch {
+
+    $ex = $_.Exception
+    Write-Host "Request to $Uri failed with HTTP Status $([int]$ex.Response.StatusCode) $($ex.Response.StatusDescription)" -f Red
+    $errorResponse = $ex.Response.GetResponseStream()
+    $reader = New-Object System.IO.StreamReader($errorResponse)
+    $reader.BaseStream.Position = 0
+    $reader.DiscardBufferedData()
+    $responseBody = $reader.ReadToEnd();
+    Write-Host "Response content:`n$responseBody" -f Red
+    Write-Error "Request to $Uri failed with HTTP Status $($ex.Response.StatusCode) $($ex.Response.StatusDescription)"
+    write-host
+    break
+
+    }
+
+}
+
+####################################################
+
+Function Get-ApplicationAssignment(){
+
+<#
+.SYNOPSIS
+This function is used to get an application assignment from the Graph API REST interface
+.DESCRIPTION
+The function connects to the Graph API Interface and gets an application assignment
+.EXAMPLE
+Get-ApplicationAssignment
+Returns an Application Assignment configured in Intune
+.NOTES
+NAME: Get-ApplicationAssignment
+#>
+
+[cmdletbinding()]
+
+param
+(
+    $ApplicationId
+)
+
+$graphApiVersion = "Beta"
+$Resource = "deviceAppManagement/mobileApps/$ApplicationId/assignments"
+
+    try {
+
+        if(!$ApplicationId){
+
+        write-host "No Application Id specified, specify a valid Application Id" -f Red
+        break
+
+        }
+
+        else {
+
+        $uri = "https://graph.microsoft.com/$graphApiVersion/$($Resource)"
+        (Invoke-RestMethod -Uri $uri -Headers $authToken -Method Get).Value
+
+        }
+
+    }
+
+    catch {
+
+    $ex = $_.Exception
+    $errorResponse = $ex.Response.GetResponseStream()
+    $reader = New-Object System.IO.StreamReader($errorResponse)
+    $reader.BaseStream.Position = 0
+    $reader.DiscardBufferedData()
+    $responseBody = $reader.ReadToEnd();
+    Write-Host "Response content:`n$responseBody" -f Red
+    Write-Error "Request to $Uri failed with HTTP Status $($ex.Response.StatusCode) $($ex.Response.StatusDescription)"
+    write-host
+    break
+
+    }
+
+}
+
+####################################################
+
+Function Get-AADGroup(){
+
+<#
+.SYNOPSIS
+This function is used to get AAD Groups from the Graph API REST interface
+.DESCRIPTION
+The function connects to the Graph API Interface and gets any Groups registered with AAD
+.EXAMPLE
+Get-AADGroup
+Returns all users registered with Azure AD
+.NOTES
+NAME: Get-AADGroup
+#>
+
+[cmdletbinding()]
+
+param
+(
+    $GroupName,
+    $id,
+    [switch]$Members
+)
+
+# Defining Variables
+$graphApiVersion = "v1.0"
+$Group_resource = "groups"
+
+    try {
+
+        if($id){
+
+        $uri = "https://graph.microsoft.com/$graphApiVersion/$($Group_resource)?`$filter=id eq '$id'"
+        (Invoke-RestMethod -Uri $uri -Headers $authToken -Method Get).Value
+
+        }
+
+        elseif($GroupName -eq "" -or $GroupName -eq $null){
+
+        $uri = "https://graph.microsoft.com/$graphApiVersion/$($Group_resource)"
+        (Invoke-RestMethod -Uri $uri -Headers $authToken -Method Get).Value
+
+        }
+
+        else {
+
+            if(!$Members){
+
+            $uri = "https://graph.microsoft.com/$graphApiVersion/$($Group_resource)?`$filter=displayname eq '$GroupName'"
+            (Invoke-RestMethod -Uri $uri -Headers $authToken -Method Get).Value
+
+            }
+
+            elseif($Members){
+
+            $uri = "https://graph.microsoft.com/$graphApiVersion/$($Group_resource)?`$filter=displayname eq '$GroupName'"
+            $Group = (Invoke-RestMethod -Uri $uri -Headers $authToken -Method Get).Value
+
+                if($Group){
+
+                $GID = $Group.id
+
+                $Group.displayName
+                write-host
+
+                $uri = "https://graph.microsoft.com/$graphApiVersion/$($Group_resource)/$GID/Members"
+                (Invoke-RestMethod -Uri $uri -Headers $authToken -Method Get).Value
+
+                }
+
+            }
+
+        }
+
+    }
+
+    catch {
+
+    $ex = $_.Exception
+    $errorResponse = $ex.Response.GetResponseStream()
+    $reader = New-Object System.IO.StreamReader($errorResponse)
+    $reader.BaseStream.Position = 0
+    $reader.DiscardBufferedData()
+    $responseBody = $reader.ReadToEnd();
+    Write-Host "Response content:`n$responseBody" -f Red
+    Write-Error "Request to $Uri failed with HTTP Status $($ex.Response.StatusCode) $($ex.Response.StatusDescription)"
+    write-host
+    break
+
+    }
+
+}
+
+####################################################
+
+#region Authentication
+
+write-host
+
+# Checking if authToken exists before running authentication
+if($global:authToken){
+
+    # Setting DateTime to Universal time to work in all timezones
+    $DateTime = (Get-Date).ToUniversalTime()
+
+    # If the authToken exists checking when it expires
+    $TokenExpires = ($authToken.ExpiresOn.datetime - $DateTime).Minutes
+
+        if($TokenExpires -le 0){
+
+        write-host "Authentication Token expired" $TokenExpires "minutes ago" -ForegroundColor Yellow
+        write-host
+
+            # Defining User Principal Name if not present
+
+            if($User -eq $null -or $User -eq ""){
+
+            $User = Read-Host -Prompt "Please specify your user principal name for Azure Authentication"
+            Write-Host
+
+            }
+
+        $global:authToken = Get-AuthToken -User $User
+
+        }
+}
+
+# Authentication doesn't exist, calling Get-AuthToken function
+
+else {
+
+    if($User -eq $null -or $User -eq ""){
+
+    $User = Read-Host -Prompt "Please specify your user principal name for Azure Authentication"
+    Write-Host
+
+    }
+
+# Getting the authorization token
+$global:authToken = Get-AuthToken -User $User
+
+}
+
+#endregion
+
+####################################################
+
+$Intune_Apps = Get-IntuneApplication | Select-Object displayName,id | Out-GridView -Title "Intune Applications" -passthru | ForEach-Object {
+
+$thisapp = get-intuneapplication -Name $_.displayName
+
+$apptype = switch($thisapp.'@odata.type') {
+"#microsoft.graph.win32LobApp" {"Win32 App"; break}
+"#microsoft.graph.microsoftStoreForBusinessApp" {"Store for Business App"; break}
+"#microsoft.graph.officeSuiteApp" {"M365 App"; break}
+"#microsoft.graph.windowsMicrosoftEdgeApp" {"Microsoft Edge"; break}
+"#microsoft.graph.windowsUniversalAppX" {"MSIX Package"; break}
+
+
+}
+
+$appname = $thisapp.displayName
+$appid =  $thisapp.id
+$apptyef =  $apptype
+
+$App_Assignment = Get-ApplicationAssignment -ApplicationId $_.id
+
+    if($App_Assignment){
+
+    $assignedtype =  "Application Assigned"
+
+        foreach($Assignment in $App_Assignment){
+
+        $assignedgroup = (Get-AADGroup -id $Assignment.target.GroupId).displayName 
+        $intent = $Assignment.intent
+
+        }
+
+    }
+    else {
+
+    $assignedtype = "No Application Assignment"
+
+    }
+
+    Write-Host
+
+$Appoutput = @"
+Name: $appname
+ID: $appid
+AppType: $apptype
+Assigned: $assignedtype
+Assigned Group(s): $assignedgroup
+Assigned Intent: $intent
+"@
+
+
+
+    [System.Windows.MessageBox]::Show($Appoutput)
+
+}
