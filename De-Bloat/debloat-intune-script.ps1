3d138dbbe47ab04f68cf91b8a6a2b3ea2719ac77
andrew-s-taylor <59832110+andrew-s-taylor@users.noreply.github.com>
2022-07-04 21:33:39 +0100

    Script updated
    Stop running software removal if Intune apps have deployed

---
+++ De-Bloat/debloat-intune-script.ps1
@@ -0,0 +1,20 @@
+$DebloatFolder = "C:\ProgramData\Debloat"
+If (Test-Path $DebloatFolder) {
+    Write-Output "$DebloatFolder exists. Skipping."
+}
+Else {
+    Write-Output "The folder '$DebloatFolder' doesn't exist. This folder will be used for storing logs created after the script runs. Creating now."
+    Start-Sleep 1
+    New-Item -Path "$DebloatFolder" -ItemType Directory
+    Write-Output "The folder $DebloatFolder was successfully created."
+}
+
+$templateFilePath = "C:\ProgramData\Debloat\removebloat.ps1"
+
+Invoke-WebRequest `
+-Uri "https://raw.githubusercontent.com/andrew-s-taylor/public/main/De-Bloat/RemoveBloat.ps1" `
+-OutFile $templateFilePath `
+-UseBasicParsing `
+-Headers @{"Cache-Control"="no-cache"}
+
+invoke-expression -Command $templateFilePath
