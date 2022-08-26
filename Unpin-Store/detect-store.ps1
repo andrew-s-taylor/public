711116476a573dfb51f4b789aac138c332207468
andrew-s-taylor <59832110+andrew-s-taylor@users.noreply.github.com>
2022-08-26 19:52:49 +0100

    Update detect-store.ps1
    Fixed remediation

--- Unpin-Store/detect-store.ps1
+++ Unpin-Store/detect-store.ps1
@@ -1,19 +1,21 @@
-$apps = ((New-Object -Com Shell.Application).NameSpace('shell:::{4234d49b-0245-4df3-b780-3893943456e1}').Items())
+##We're looping through the verbs so it's going to be easier to count
+$pinned = 0
+##Loop through verbs for the store app
+$apps = ((New-Object -Com Shell.Application).NameSpace('shell:::{4234d49b-0245-4df3-b780-3893943456e1}').Items() | Where-Object { $_.Name -eq "Microsoft Store" }).verbs()
 foreach ($app in $apps) {
-$appname = $app.Name
-if ($appname -like "*store*") {
-$finalname = $app.Name
-}
-else {
-    $finalname = ""
+    ##Is Unpin an option?
+if ($app.Name -eq "Unpin from tas&kbar") {
+    ##Yep, increment the counter
+$pinned++
 }
 }
 
-if ($finalname -eq "") {
-    write-host "Store Not Pinned"
-    exit 1
+#Has it been found?
+if ($pinned -gt 0) {
+Write-Warning "Store has been pinned"
+exit 1
 }
 else {
-    Write-Warning "Store Pinned, Remediating"
-    exit 0
+write-host "Not pinned"
+exit 0
 }
