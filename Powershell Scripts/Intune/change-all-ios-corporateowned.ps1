02cb219fd2d22b15c474d56b8ff8790f53d4fdfc
andrew-s-taylor <andrew@tinymice.co.uk>
2022-09-21 21:20:27 +0100

    Create change-all-ios-corporateowned.ps1

---
+++ Powershell Scripts/Intune/change-all-ios-corporateowned.ps1
@@ -0,0 +1,306 @@
+﻿function Get-AuthToken {
+
+    <#
+    .SYNOPSIS
+    This function is used to authenticate with the Graph API REST interface
+    .DESCRIPTION
+    The function authenticate with the Graph API Interface with the tenant name
+    .EXAMPLE
+    Get-AuthToken
+    Authenticates you with the Graph API interface
+    .NOTES
+    NAME: Get-AuthToken
+    #>
+    
+    [cmdletbinding()]
+    
+    param
+    (
+        [Parameter(Mandatory=$true)]
+        $User
+    )
+    
+    $userUpn = New-Object "System.Net.Mail.MailAddress" -ArgumentList $User
+    
+    $tenant = $userUpn.Host
+    
+    Write-Host "Checking for AzureAD module..."
+    
+        $AadModule = Get-Module -Name "AzureADPreview" -ListAvailable
+    
+        if ($AadModule -eq $null) {
+    
+            Write-Host "AzureAD PowerShell module not found, looking for AzureADPreview"
+            $AadModule = Get-Module -Name "AzureADPreview" -ListAvailable
+    
+        }
+    
+        if ($AadModule -eq $null) {
+            write-host
+            write-host "AzureAD Powershell module not installed..." -f Red
+            write-host "Install by running 'Install-Module AzureAD' or 'Install-Module AzureADPreview' from an elevated PowerShell prompt" -f Yellow
+            write-host "Script can't continue..." -f Red
+            write-host
+            exit
+        }
+    
+    # Getting path to ActiveDirectory Assemblies
+    # If the module count is greater than 1 find the latest version
+    
+        if($AadModule.count -gt 1){
+    
+            $Latest_Version = ($AadModule | select version | Sort-Object)[-1]
+    
+            $aadModule = $AadModule | ? { $_.version -eq $Latest_Version.version }
+    
+                # Checking if there are multiple versions of the same module found
+    
+                if($AadModule.count -gt 1){
+    
+                $aadModule = $AadModule | select -Unique
+    
+                }
+    
+            $adal = Join-Path $AadModule.ModuleBase "Microsoft.IdentityModel.Clients.ActiveDirectory.dll"
+            $adalforms = Join-Path $AadModule.ModuleBase "Microsoft.IdentityModel.Clients.ActiveDirectory.Platform.dll"
+    
+        }
+    
+        else {
+    
+            $adal = Join-Path $AadModule.ModuleBase "Microsoft.IdentityModel.Clients.ActiveDirectory.dll"
+            $adalforms = Join-Path $AadModule.ModuleBase "Microsoft.IdentityModel.Clients.ActiveDirectory.Platform.dll"
+    
+        }
+    
+    [System.Reflection.Assembly]::LoadFrom($adal) | Out-Null
+    
+    [System.Reflection.Assembly]::LoadFrom($adalforms) | Out-Null
+    
+    $clientId = "d1ddf0e4-d672-4dae-b554-9d5bdfd93547"
+    
+    $redirectUri = "urn:ietf:wg:oauth:2.0:oob"
+    
+    $resourceAppIdURI = "https://graph.microsoft.com"
+    
+    $authority = "https://login.microsoftonline.com/$Tenant"
+    
+        try {
+    
+        $authContext = New-Object "Microsoft.IdentityModel.Clients.ActiveDirectory.AuthenticationContext" -ArgumentList $authority
+    
+        # https://msdn.microsoft.com/en-us/library/azure/microsoft.identitymodel.clients.activedirectory.promptbehavior.aspx
+        # Change the prompt behaviour to force credentials each time: Auto, Always, Never, RefreshSession
+    
+        $platformParameters = New-Object "Microsoft.IdentityModel.Clients.ActiveDirectory.PlatformParameters" -ArgumentList "Auto"
+    
+        $userId = New-Object "Microsoft.IdentityModel.Clients.ActiveDirectory.UserIdentifier" -ArgumentList ($User, "OptionalDisplayableId")
+    
+        $authResult = $authContext.AcquireTokenAsync($resourceAppIdURI,$clientId,$redirectUri,$platformParameters,$userId).Result
+    
+            # If the accesstoken is valid then create the authentication header
+    
+            if($authResult.AccessToken){
+    
+            # Creating header for Authorization token
+    
+            $authHeader = @{
+                'Content-Type'='application/json'
+                'Authorization'="Bearer " + $authResult.AccessToken
+                'ExpiresOn'=$authResult.ExpiresOn
+                'ConsistencyLevel' = 'eventual'
+                }
+    
+            return $authHeader
+    
+            }
+    
+            else {
+    
+            Write-Host
+            Write-Host "Authorization Access Token is null, please re-run authentication..." -ForegroundColor Red
+            Write-Host
+            break
+    
+            }
+    
+        }
+    
+        catch {
+    
+        write-host $_.Exception.Message -f Red
+        write-host $_.Exception.ItemName -f Red
+        write-host
+        break
+    
+        }
+    
+    }
+    
+
+Function Set-ManagedDevice(){
+
+<#
+.SYNOPSIS
+This function is used to set Managed Device property from the Graph API REST interface
+.DESCRIPTION
+The function connects to the Graph API Interface and sets a Managed Device property
+.EXAMPLE
+Set-ManagedDevice -id $id -ownerType company
+Returns Managed Devices configured in Intune
+.NOTES
+NAME: Set-ManagedDevice
+#>
+
+[cmdletbinding()]
+
+param
+(
+    $id,
+    $ownertype
+)
+
+
+$graphApiVersion = "Beta"
+$Resource = "deviceManagement/managedDevices"
+
+    try {
+
+        if($id -eq "" -or $id -eq $null){
+
+        write-host "No Device id specified, please provide a device id..." -f Red
+        break
+
+        }
+        
+        if($ownerType -eq "" -or $ownerType -eq $null){
+
+            write-host "No ownerType parameter specified, please provide an ownerType. Supported value personal or company..." -f Red
+            Write-Host
+            break
+
+            }
+
+        elseif($ownerType -eq "company"){
+
+$JSON = @"
+
+{
+    ownerType:"company"
+}
+
+"@
+
+
+            
+                # Send Patch command to Graph to change the ownertype
+                $uri = "https://graph.microsoft.com/beta/deviceManagement/managedDevices('$ID')"
+                Invoke-RestMethod -Uri $uri -Headers $authToken -Method Patch -Body $Json -ContentType "application/json"
+
+            
+            }
+
+        elseif($ownerType -eq "personal"){
+
+$JSON = @"
+
+{
+    ownerType:"personal"
+}
+
+"@
+
+
+            
+                # Send Patch command to Graph to change the ownertype
+                $uri = "https://graph.microsoft.com/beta/deviceManagement/managedDevices('$ID')"
+                Invoke-RestMethod -Uri $uri -Headers $authToken -Method Patch -Body $Json -ContentType "application/json"
+
+
+            }
+
+    }
+
+    catch {
+
+    $ex = $_.Exception
+    $errorResponse = $ex.Response.GetResponseStream()
+    $reader = New-Object System.IO.StreamReader($errorResponse)
+    $reader.BaseStream.Position = 0
+    $reader.DiscardBufferedData()
+    $responseBody = $reader.ReadToEnd();
+    Write-Host "Response content:`n$responseBody" -f Red
+    Write-Error "Request to $Uri failed with HTTP Status $($ex.Response.StatusCode) $($ex.Response.StatusDescription)"
+    write-host
+    break
+
+    }
+
+}
+
+###############################################################################################################
+######                                          MS Graph Implementations                                 ######
+###############################################################################################################
+##Get Source Creds
+$credential = Get-Credential -Message "Enter your tenant details"
+$user = $credential.username
+
+#Authenticate for MS Graph
+#region Authentication
+
+
+# Checking if authToken exists before running authentication
+if($global:authToken){
+
+    # Setting DateTime to Universal time to work in all timezones
+    $DateTime = (Get-Date).ToUniversalTime()
+
+    # If the authToken exists checking when it expires
+    $TokenExpires = ($authToken.ExpiresOn.datetime - $DateTime).Minutes
+
+        if($TokenExpires -le 0){
+
+        write-host "Authentication Token expired" $TokenExpires "minutes ago" -ForegroundColor Yellow
+
+            # Defining User Principal Name if not present
+
+            if($User -eq $null -or $User -eq ""){
+
+            $User = Read-Host -Prompt "Please specify your user principal name for Azure Authentication"
+
+            }
+
+        $global:authToken = Get-AuthToken -User $credential.UserName
+
+        }
+}
+
+# Authentication doesn't exist, calling Get-AuthToken function
+
+else {
+
+    if($User -eq $null -or $User -eq ""){
+
+    $User = Read-Host -Prompt "Please specify your user principal name for Azure Authentication"
+    Write-Host
+
+    }
+
+# Getting the authorization token
+
+#$tenant = Read-Host -Prompt "Please specify your tenant email address"
+$global:authToken = Get-AuthToken -User $credential.UserName
+
+}
+
+#endregion
+
+
+$uri = "https://graph.microsoft.com/beta/deviceManagement/managedDevices?`$filter=deviceType eq 'iPhone'"
+$iphones = (Invoke-RestMethod -Uri $uri -Headers $authToken -Method Get).Value
+foreach ($iphone in $iphones) {
+$phoneid = $iphone.id
+write-host "Setting $phoneid to Corporate Owned"
+    Set-ManagedDevice -id $phoneid -ownertype company
+
+}
