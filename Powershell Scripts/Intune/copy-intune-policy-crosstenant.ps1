7e0951fccbb1a99c14ef1994f84b73ff67d8a7cd
andrew-s-taylor <andrew@tinymice.co.uk>
2023-02-03 22:19:34 +0000

    versioning

--- Powershell Scripts/Intune/copy-intune-policy-crosstenant.ps1
+++ Powershell Scripts/Intune/copy-intune-policy-crosstenant.ps1
@@ -1,6 +1,6 @@
 #[System.Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseSingularNouns', '', Scope='Function', Target='Get-MSGraphAllPages')]
 <#PSScriptInfo
-.VERSION 5.0.4
+.VERSION 5.0.5
 .GUID ec2a6c43-35ad-48cd-b23c-da987f1a528b
 .AUTHOR AndrewTaylor
 .DESCRIPTION Copies any Intune Policy via Microsoft Graph to "Copy of (policy name)".  Displays list of policies using GridView to select which to copy.  Cross tenant version
@@ -26,7 +26,7 @@ None
 .OUTPUTS
 Creates a log file in %Temp%
 .NOTES
-  Version:        5.0.4
+  Version:        5.0.5
   Author:         Andrew Taylor
   WWW:            andrewstaylor.com
   Creation Date:  25/07/2022
