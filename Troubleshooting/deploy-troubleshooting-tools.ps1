2be28b98c5cce788aea1fc3d4a78fcb9d3295be1
andrew-s-taylor <andrew@tinymice.co.uk>
2023-07-03 11:18:11 +0100

    Update to launch automatically

--- Troubleshooting/deploy-troubleshooting-tools.ps1
+++ Troubleshooting/deploy-troubleshooting-tools.ps1
@@ -1,5 +1,5 @@
 <#PSScriptInfo
-.VERSION 1.1.0
+.VERSION 1.1.1
 .GUID 600c7e1b-44a5-4aaa-9644-b2763b9ccc5e
 .AUTHOR AndrewTaylor
 .DESCRIPTION Downloads and deploys troubleshooting tools to display in Autopilot ESP
@@ -25,14 +25,15 @@ None required
 .OUTPUTS
 None required
 .NOTES
-  Version:        1.1.0
+  Version:        1.1.1
   Author:         Andrew Taylor
   Twitter:        @AndrewTaylor_2
   WWW:            andrewstaylor.com
   Creation Date:  03/08/2022
-  Updated Date: 28/08/2022
+  Updated Date: 03/07/2023
   Purpose/Change: Initial script development
   Change: Added logic to stop running outside OOBE
+  Change: Added command to auto-launch tools.  Thanks to Flo @ sunrise-it.fr
 
   
 .EXAMPLE
@@ -118,17 +119,33 @@ Invoke-WebRequest `
 -Headers @{"Cache-Control"="no-cache"}
 
 ##Create powershell script we are launching
+##$string = @"
+##[void][System.Reflection.Assembly]::LoadWithPartialName('System.Windows.Forms')
+##[System.Windows.Forms.SendKeys]::SendWait("+{f10}") 
+##start-process powershell.exe -argument '-nologo -noprofile -noexit -executionpolicy bypass -command C:\ProgramData\ServiceUI\tools.ps1 ' -Wait
+##"@
+
+##$file2="C:\ProgramData\ServiceUI\shiftf10.ps1"
+##$string | out-file $file2
+
+##Create powershell script we are launching
 $string = @"
-[void][System.Reflection.Assembly]::LoadWithPartialName('System.Windows.Forms')
-[System.Windows.Forms.SendKeys]::SendWait("+{f10}") 
+# Send Shift+F10 key to open a command prompt
+$WscriptShell = New-Object -ComObject Wscript.Shell
+$WscriptShell.SendKeys("%({TAB})")
+Start-Sleep 1
+$WscriptShell.SendKeys("+({F10})")
+# Wait until cmd process is opened
+Do {Start-Sleep 1} While (-not (Get-Process cmd -ErrorAction SilentlyContinue))
+Start-Sleep 1
+Get-Process cmd | Stop-Process -Force
 start-process powershell.exe -argument '-nologo -noprofile -noexit -executionpolicy bypass -command C:\ProgramData\ServiceUI\tools.ps1 ' -Wait
 "@
-
 $file2="C:\ProgramData\ServiceUI\shiftf10.ps1"
 $string | out-file $file2
 
 ##Check if we're during OOBE
-$intunepath = “HKLM:\SOFTWARE\Microsoft\IntuneManagementExtension\Win32Apps”
+$intunepath = "HKLM:\SOFTWARE\Microsoft\IntuneManagementExtension\Win32Apps"
 $intunecomplete = @(Get-ChildItem $intunepath).count
 if ($intunecomplete -lt 2) {
 
