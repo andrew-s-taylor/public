f60bedf8c5cf37d879aae3737a7a3a2651371653
andrew-s-taylor <andrew@tinymice.co.uk>
2023-09-26 14:14:16 +0100

    New script

---
+++ Powershell Scripts/list-uninstall-keys.ps1
@@ -0,0 +1,69 @@
+﻿##Search for 32-bit versions and list them
+$allstring = @()
+$path1 =  "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall"
+#Loop Through the apps if name has Adobe and NOT reader
+$32apps = Get-ChildItem -Path $path1 | Get-ItemProperty | Select-Object -Property DisplayName, UninstallString
+
+foreach ($32app in $32apps) {
+#Get uninstall string
+$string1 =  $32app.uninstallstring
+#Check if it's an MSI install
+if ($string1 -match "^msiexec*") {
+#MSI install, replace the I with an X and make it quiet
+$string2 = $string1 + " /quiet /norestart"
+$string2 = $string2 -replace "/I", "/X "
+#Remove msiexec as we need to split for the uninstall
+$string2 = $string2 -replace "msiexec.exe", ""
+#Create custom object with name and string
+$allstring += New-Object -TypeName PSObject -Property @{
+    Name = $32app.DisplayName
+    String = $string2
+}
+}
+else {
+#Exe installer, run straight path
+$string2 = $string1
+$allstring += New-Object -TypeName PSObject -Property @{
+    Name = $32app.DisplayName
+    String = $string2
+}
+}
+
+}
+
+
+##Search for 64-bit versions and list them
+
+$path2 =  "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall"
+#Loop Through the apps if name has Adobe and NOT reader
+$64apps = Get-ChildItem -Path $path2 | Get-ItemProperty | Select-Object -Property DisplayName, UninstallString
+
+foreach ($64app in $64apps) {
+#Get uninstall string
+$string1 =  $64app.uninstallstring
+#Check if it's an MSI install
+if ($string1 -match "^msiexec*") {
+#MSI install, replace the I with an X and make it quiet
+$string2 = $string1 + " /quiet /norestart"
+$string2 = $string2 -replace "/I", "/X "
+#Remove msiexec as we need to split for the uninstall
+$string2 = $string2 -replace "msiexec.exe", ""
+#Uninstall with string2 params
+$allstring += New-Object -TypeName PSObject -Property @{
+    Name = $64app.DisplayName
+    String = $string2
+}
+}
+else {
+#Exe installer, run straight path
+$string2 = $string1
+$allstring += New-Object -TypeName PSObject -Property @{
+    Name = $64app.DisplayName
+    String = $string2
+}
+}
+
+}
+
+
+$allstring | Out-GridView
