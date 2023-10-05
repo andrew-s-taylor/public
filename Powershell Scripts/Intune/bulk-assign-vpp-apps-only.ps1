03cd25eb3bbd34f0684486b18a567a14575903f1
andrew-s-taylor <andrew@tinymice.co.uk>
2023-10-05 20:59:50 +0100

    New script

---
+++ Powershell Scripts/Intune/bulk-assign-vpp-apps-only.ps1
@@ -0,0 +1,172 @@
+$ErrorActionPreference = "Continue"
+##Start Logging to %TEMP%\intune.log
+$date = get-date -format ddMMyyyy
+Start-Transcript -Path $env:TEMP\intune-$date.log
+###############################################################################################################
+######                                         Install Modules                                           ######
+###############################################################################################################
+Write-Host "Installing Microsoft Graph modules if required (current user scope)"
+#Install MS Graph if not available
+if (Get-Module -ListAvailable -Name Microsoft.Graph) {
+    Write-Host "Microsoft Graph Already Installed"
+} 
+else {
+    try {
+        Install-Module -Name Microsoft.Graph.authentication -Scope CurrentUser -Repository PSGallery -Force 
+    }
+    catch [Exception] {
+        $_.message 
+        exit
+    }
+}
+# Load the Graph module
+Import-Module microsoft.graph.authentication  
+###############################################################################################################
+######                                          Add Functions                                            ######
+###############################################################################################################
+
+
+function getallpagination () {
+    <#
+.SYNOPSIS
+This function is used to grab all items from Graph API that are paginated
+.DESCRIPTION
+The function connects to the Graph API Interface and gets all items from the API that are paginated
+.EXAMPLE
+getallpagination -url "https://graph.microsoft.com/v1.0/groups"
+ Returns all items
+.NOTES
+ NAME: getallpagination
+#>
+[cmdletbinding()]
+    
+param
+(
+    $url
+)
+    $response = (Invoke-MgGraphRequest -uri $url -Method Get -OutputType PSObject)
+    $alloutput = $response.value
+    
+    $alloutputNextLink = $response."@odata.nextLink"
+    
+    while ($null -ne $alloutputNextLink) {
+        $alloutputResponse = (Invoke-MGGraphRequest -Uri $alloutputNextLink -Method Get -outputType PSObject)
+        $alloutputNextLink = $alloutputResponse."@odata.nextLink"
+        $alloutput += $alloutputResponse.value
+    }
+    
+    return $alloutput
+    }
+
+Function Connect-ToGraph {
+    <#
+.SYNOPSIS
+Authenticates to the Graph API via the Microsoft.Graph.Authentication module.
+ 
+.DESCRIPTION
+The Connect-ToGraph cmdlet is a wrapper cmdlet that helps authenticate to the Intune Graph API using the Microsoft.Graph.Authentication module. It leverages an Azure AD app ID and app secret for authentication or user-based auth.
+ 
+.PARAMETER Tenant
+Specifies the tenant (e.g. contoso.onmicrosoft.com) to which to authenticate.
+ 
+.PARAMETER AppId
+Specifies the Azure AD app ID (GUID) for the application that will be used to authenticate.
+ 
+.PARAMETER AppSecret
+Specifies the Azure AD app secret corresponding to the app ID that will be used to authenticate.
+
+.PARAMETER Scopes
+Specifies the user scopes for interactive authentication.
+ 
+.EXAMPLE
+Connect-ToGraph -TenantId $tenantID -AppId $app -AppSecret $secret
+ 
+-#>
+    [cmdletbinding()]
+    param
+    (
+        [Parameter(Mandatory = $false)] [string]$Tenant,
+        [Parameter(Mandatory = $false)] [string]$AppId,
+        [Parameter(Mandatory = $false)] [string]$AppSecret,
+        [Parameter(Mandatory = $false)] [string]$scopes
+    )
+
+    Process {
+        Import-Module Microsoft.Graph.Authentication
+        $version = (get-module microsoft.graph.authentication | Select-Object -expandproperty Version).major
+
+        if ($AppId -ne "") {
+            $body = @{
+                grant_type    = "client_credentials";
+                client_id     = $AppId;
+                client_secret = $AppSecret;
+                scope         = "https://graph.microsoft.com/.default";
+            }
+     
+            $response = Invoke-RestMethod -Method Post -Uri https://login.microsoftonline.com/$Tenant/oauth2/v2.0/token -Body $body
+            $accessToken = $response.access_token
+     
+            $accessToken
+            if ($version -eq 2) {
+                write-host "Version 2 module detected"
+                $accesstokenfinal = ConvertTo-SecureString -String $accessToken -AsPlainText -Force
+            }
+            else {
+                write-host "Version 1 Module Detected"
+                Select-MgProfile -Name Beta
+                $accesstokenfinal = $accessToken
+            }
+            $graph = Connect-MgGraph  -AccessToken $accesstokenfinal 
+            Write-Host "Connected to Intune tenant $TenantId using app-based authentication (Azure AD authentication not supported)"
+        }
+        else {
+            if ($version -eq 2) {
+                write-host "Version 2 module detected"
+            }
+            else {
+                write-host "Version 1 Module Detected"
+                Select-MgProfile -Name Beta
+            }
+            $graph = Connect-MgGraph -scopes $scopes
+            Write-Host "Connected to Intune tenant $($graph.TenantId)"
+        }
+    }
+}    
+###############################################################################################################
+######                                          Launch Form                                              ######
+###############################################################################################################
+#Connect to Graph
+Connect-ToGraph -Scopes "Domain.Read.All, Directory.Read.All, DeviceManagementApps.ReadWrite.All, openid, profile, email, offline_access"
+
+$allapps = getallpagination -url "https://graph.microsoft.com/beta/deviceAppManagement/mobileApps" | Where-Object '@odata.type' -eq "#microsoft.graph.iosVppApp"
+
+foreach ($app in $allapps) {
+    $appname = $app.displayName
+    write-host "Assigning $appname"
+    $appid = $app.id
+    $url = "https://graph.microsoft.com/beta/deviceAppManagement/mobileApps/$appid/assign"
+
+$json = @"
+{
+	"mobileAppAssignments": [
+		{
+			"@odata.type": "#microsoft.graph.mobileAppAssignment",
+			"intent": "Required",
+			"settings": {
+				"@odata.type": "#microsoft.graph.iosVppAppAssignmentSettings",
+				"isRemovable": true,
+				"preventAutoAppUpdate": false,
+				"preventManagedAppBackup": false,
+				"uninstallOnDeviceRemoval": false,
+				"useDeviceLicensing": true,
+				"vpnConfigurationId": null
+			},
+			"target": {
+				"@odata.type": "#microsoft.graph.allLicensedUsersAssignmentTarget"
+			}
+		}
+	]
+}
+"@
+Invoke-MgGraphRequest -Uri $url -Method POST -Body $json -ContentType "application/json"
+}
