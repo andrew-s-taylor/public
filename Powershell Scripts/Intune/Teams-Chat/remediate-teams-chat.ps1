914b947dcf0f3159fe0ef52305ee67d2b76398fc
andrew-s-taylor <andrew@tinymice.co.uk>
2023-10-06 13:34:21 +0100

    Fix

--- Powershell Scripts/Intune/Teams-Chat/remediate-teams-chat.ps1
+++ Powershell Scripts/Intune/Teams-Chat/remediate-teams-chat.ps1
@@ -5,12 +5,12 @@ $WinPackage = Get-AppxPackage -allusers | Where-Object {$_.Name -eq $MSTeams}
 $ProvisionedPackage = Get-AppxProvisionedPackage -Online | Where-Object { $_.DisplayName -eq $WinPackage }
 If ($null -ne $WinPackage) 
 {
-    Remove-AppxPackage  -Package $WinPackage.PackageFullName
+    Remove-AppxPackage  -Package $WinPackage.PackageFullName -AllUsers
 } 
 
 If ($null -ne $ProvisionedPackage) 
 {
-    Remove-AppxProvisionedPackage -online -Packagename $ProvisionedPackage.Packagename
+    Remove-AppxProvisionedPackage -online -Packagename $ProvisionedPackage.Packagename -AllUsers
 }
 
 ##Tweak reg permissions
