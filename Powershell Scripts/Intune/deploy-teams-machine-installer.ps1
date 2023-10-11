2c75746f90f29d00605dd447e6d8970449207ae0
andrew-s-taylor <andrew@tinymice.co.uk>
2023-10-11 11:42:07 +0100

    Added Graph connection

--- Powershell Scripts/Intune/deploy-teams-machine-installer.ps1
+++ Powershell Scripts/Intune/deploy-teams-machine-installer.ps1
@@ -1,5 +1,5 @@
 <#PSScriptInfo
-.VERSION 1.0.0
+.VERSION 1.0.1
 .GUID 94357caa-de53-401c-bbdd-2400a974944c
 .AUTHOR AndrewTaylor
 .DESCRIPTION Packages and deploys Teams Machine Wide installer to Intune
@@ -29,7 +29,7 @@ None
 .OUTPUTS
 In-Line Outputs
 .NOTES
-  Version:        1.0.0
+  Version:        1.0.1
   Author:         Andrew Taylor
   Twitter:        @AndrewTaylor_2
   WWW:            andrewstaylor.com
@@ -38,6 +38,18 @@ In-Line Outputs
 .EXAMPLE
 N/A
 #>
+
+[cmdletbinding()]
+    
+param
+(
+    [string]$tenant #Tenant ID (optional) for when automating and you want to use across tenants instead of hard-coded
+    ,
+    [string]$clientid #ClientID is the type of Azure AD App Reg ID
+    ,
+    [string]$clientsecret #ClientSecret is the type of Azure AD App Reg Secret
+
+    )
 
 $url = "https://teams.microsoft.com/desktopclient/installer/windows/x64"
 
@@ -69,12 +81,100 @@ $intuneapputilurl = "https://github.com/microsoft/Microsoft-Win32-Content-Prep-T
 $intuneapputiloutput = $path + "IntuneWinAppUtil.exe"
 Invoke-WebRequest -Uri $intuneapputilurl -OutFile $intuneapputiloutput
 
+###############################################################################################################
+######                                         Install Modules                                           ######
+###############################################################################################################
+Write-Host "Installing Intune modules if required (current user scope)"
 
+#Install MS Graph if not available
+if (Get-Module -ListAvailable -Name Microsoft.Graph.Authentication) {
+    Write-Host "Microsoft Graph Already Installed"
+} 
+else {
+
+        Install-Module -Name Microsoft.Graph.Authentication -Scope CurrentUser -Repository PSGallery -Force 
+
+}
+
+import-module microsoft.graph.authentication
 
 
 
 ################################################# START FUNCTIONS ###################################################################
+Function Connect-ToGraph {
+    <#
+.SYNOPSIS
+Authenticates to the Graph API via the Microsoft.Graph.Authentication module.
+ 
+.DESCRIPTION
+The Connect-ToGraph cmdlet is a wrapper cmdlet that helps authenticate to the Intune Graph API using the Microsoft.Graph.Authentication module. It leverages an Azure AD app ID and app secret for authentication or user-based auth.
+ 
+.PARAMETER Tenant
+Specifies the tenant (e.g. contoso.onmicrosoft.com) to which to authenticate.
+ 
+.PARAMETER AppId
+Specifies the Azure AD app ID (GUID) for the application that will be used to authenticate.
+ 
+.PARAMETER AppSecret
+Specifies the Azure AD app secret corresponding to the app ID that will be used to authenticate.
 
+.PARAMETER Scopes
+Specifies the user scopes for interactive authentication.
+ 
+.EXAMPLE
+Connect-ToGraph -TenantId $tenantID -AppId $app -AppSecret $secret
+ 
+-#>
+    [cmdletbinding()]
+    param
+    (
+        [Parameter(Mandatory = $false)] [string]$Tenant,
+        [Parameter(Mandatory = $false)] [string]$AppId,
+        [Parameter(Mandatory = $false)] [string]$AppSecret,
+        [Parameter(Mandatory = $false)] [string]$scopes
+    )
+
+    Process {
+        Import-Module Microsoft.Graph.Authentication
+        $version = (get-module microsoft.graph.authentication | Select-Object -expandproperty Version).major
+
+        if ($AppId -ne "") {
+            $body = @{
+                grant_type    = "client_credentials";
+                client_id     = $AppId;
+                client_secret = $AppSecret;
+                scope         = "https://graph.microsoft.com/.default";
+            }
+     
+            $response = Invoke-RestMethod -Method Post -Uri https://login.microsoftonline.com/$Tenant/oauth2/v2.0/token -Body $body
+            $accessToken = $response.access_token
+     
+            $accessToken
+            if ($version -eq 2) {
+                write-host "Version 2 module detected"
+                $accesstokenfinal = ConvertTo-SecureString -String $accessToken -AsPlainText -Force
+            }
+            else {
+                write-host "Version 1 Module Detected"
+                Select-MgProfile -Name Beta
+                $accesstokenfinal = $accessToken
+            }
+            $graph = Connect-MgGraph  -AccessToken $accesstokenfinal 
+            Write-Host "Connected to Intune tenant $TenantId using app-based authentication (Azure AD authentication not supported)"
+        }
+        else {
+            if ($version -eq 2) {
+                write-host "Version 2 module detected"
+            }
+            else {
+                write-host "Version 1 Module Detected"
+                Select-MgProfile -Name Beta
+            }
+            $graph = Connect-MgGraph -scopes $scopes
+            Write-Host "Connected to Intune tenant $($graph.TenantId)"
+        }
+    }
+}    
 function getallpagination () {
     <#
 .SYNOPSIS
@@ -1067,7 +1167,21 @@ function new-win32app {
 }
 
 ################################################# END FUNCTIONS ###################################################################
+###############################################################################################################
+######                                        Graph Connection                                           ######
+###############################################################################################################
 
+Write-Verbose "Connecting to Microsoft Graph"
+
+if ($clientid -and $clientsecret -and $tenant) {
+Connect-ToGraph -Tenant $tenant -AppId $clientid -AppSecret $clientsecret
+write-output "Graph Connection Established"
+}
+else {
+##Connect to Graph
+Connect-ToGraph -Scopes "DeviceManagementApps.ReadWrite.All, DeviceManagementConfiguration.ReadWrite.All, Group.ReadWrite.All, GroupMember.ReadWrite.All, openid, profile, email, offline_access"
+}
+Write-Verbose "Graph connection established"
 
 
     ##Create Directory
