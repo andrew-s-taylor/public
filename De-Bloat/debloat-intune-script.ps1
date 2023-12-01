785ef493679e968f37d703c494f07ac6e32c3e61
CITS-RED <126930382+CITS-RED@users.noreply.github.com>
2023-12-01 15:59:40 -0700

    Update debloat-intune-script.ps1
    Changed to point to my repository

--- De-Bloat/debloat-intune-script.ps1
+++ De-Bloat/debloat-intune-script.ps1
@@ -12,7 +12,7 @@ Else {
 $templateFilePath = "C:\ProgramData\Debloat\removebloat.ps1"
 
 Invoke-WebRequest `
--Uri "https://raw.githubusercontent.com/andrew-s-taylor/public/main/De-Bloat/RemoveBloat.ps1" `
+-Uri "https://github.com/CanyonITS/Scripts/blob/main/De-Bloat/RemoveBloat.ps1" `
 -OutFile $templateFilePath `
 -UseBasicParsing `
 -Headers @{"Cache-Control"="no-cache"}
